f34cae412d30f0b8af4c8437c62e0c86bbbbc493
dennis <dennis.mwangi@student.moringaschool.com>
2023-07-22 23:15:06 +0300

    Completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
 // Code EyesOnMe Component Here
+// import { useState } from "react"
+import React, {useState}  from "react"
+
+function EyesOnMe () {
+   const [isFocus, setIsFocus] = useState(false)
+   console.log({isFocus, setIsFocus})
+
+function handleClick (event) {
+    setIsFocus(!isFocus)
+
+    // (isFocus ? "Good!": "Hey! Eyes on me!")
+
+}
+
+    return (
+    <div>
+        <button onClick={handleClick}>{isFocus ? "Good!": "Hey! Eyes on me!"}</button>
+    </div>
+    )
+}
+
+export default EyesOnMe
